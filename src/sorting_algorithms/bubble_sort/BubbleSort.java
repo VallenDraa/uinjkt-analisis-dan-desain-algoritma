a4d691ce2a60198edf46a33eb2eb8e815a329fae
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-03-24 13:30:25 +0700

    improve bubble sort

--- src/sorting_algorithms/bubble_sort/BubbleSort.java
+++ src/sorting_algorithms/bubble_sort/BubbleSort.java
@@ -1,16 +1,27 @@
 package sorting_algorithms.bubble_sort;
 
 public class BubbleSort {
+  // An optimized version of Bubble Sort
   public static Integer[] sort(Integer[] array) {
-    Integer[] result = (Integer[]) array.clone();
+    Integer[] result = array.clone();
 
-    for (int i = 0; i < result.length; i++) {
-      for (int j = 0; j < result.length - 1; j++) {
+    for (int i = 0; i < array.length - 1; i++) {
+      boolean swapped = false;
+
+      for (int j = 0; j < array.length - i - 1; j++) {
         if (result[j] > result[j + 1]) {
+          // Swap result[j] and result[j+1]
           int temp = result[j];
           result[j] = result[j + 1];
           result[j + 1] = temp;
+          swapped = true;
         }
+      }
+
+      // If no two elements were
+      // swapped by inner loop, then break
+      if (swapped == false) {
+        break;
       }
     }
 
